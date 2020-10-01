c10ad3421899de1750a4fd04165ec05d6c891889
Jean Saade <>
2020-10-01 20:53:45 +0300

    latest

--- src/main/java/com/bourse/repositories/SovereignYieldsRepository.java
+++ src/main/java/com/bourse/repositories/SovereignYieldsRepository.java
@@ -18,61 +18,69 @@ public interface SovereignYieldsRepository extends JpaRepository<SovereignData, 
 	 * List<SovereignData> findAllClothsUrl( Pageable page);
 	 */
 	
-	 @Query(value = "select refer_date as x,\r\n" + 
+	 @Query(value = "select STR_TO_DATE(refer_date,'%d-%m-%Y') as x,\r\n" + 
 			              " five_yr_factor as y\r\n" + 
 		 		"from `sovereign_data`\r\n" + 
-		 		"where subgroup_id =:subGroupId",
+		 		"where subgroup_id =:subGroupId "
+		 		+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 	               nativeQuery = true)
 	 public List<DataGraphDTO> findGraphDataForFiveBySubroupId(@Param("subGroupId") long subGroupId);
 	 
 	 
 	 
-	 @Query(value = "select two_yr_factor/five_yr_factor as y,\r\n" + 
-						"       refer_date as x\r\n" + 
+	 @Query(value = "select STR_TO_DATE(refer_date,'%d-%m-%Y') as x,\r\n" + 
+						"      two_yr_factor/five_yr_factor as y  \r\n" + 
 						"  from `sovereign_data`\r\n" + 
-						"  where subgroup_id=:subGroupId",
+						"  where subgroup_id=:subGroupId "
+						+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 					      nativeQuery = true)
 	 public List<DataGraphDTO> findGraphDataForTwoOverFiveBySubroupId(@Param("subGroupId") long subGroupId);
 	 
-	 @Query(value = "select two_yr_factor/ten_yr_factor as y,\r\n" + 
-				"       refer_date as x\r\n" + 
+	 @Query(value = "select STR_TO_DATE(refer_date,'%d-%m-%Y') as x,\r\n" + 
+				"       two_yr_factor/ten_yr_factor as y\r\n" + 
 				"  from `sovereign_data`\r\n" + 
-				"  where subgroup_id=:subGroupId",
+				"  where subgroup_id=:subGroupId "
+				+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 			      nativeQuery = true)
      public List<DataGraphDTO> findGraphDataForTwoOverTenBySubroupId(@Param("subGroupId") long subGroupId);
 	 
-	 @Query(value = "select five_yr_factor/ten_yr_factor as y,\r\n" + 
-				"       refer_date as x\r\n" + 
+	 @Query(value = "select STR_TO_DATE(refer_date,'%d-%m-%Y') as x,\r\n" + 
+				"       five_yr_factor/ten_yr_factor as y\r\n" + 
 				"  from `sovereign_data`\r\n" + 
-				"  where subgroup_id=:subGroupId",
+				"  where subgroup_id=:subGroupId "
+				+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 			      nativeQuery = true)
      public List<DataGraphDTO> findGraphDataForFiveOverTenBySubroupId(@Param("subGroupId") long subGroupId);
 	 
-	 @Query(value = "select ten_yr_factor/thirtee_yr_factor as y,\r\n" + 
-				"       refer_date as x\r\n" + 
+	 @Query(value = "select STR_TO_DATE(refer_date,'%d-%m-%Y') as x ,\r\n" + 
+				"      ten_yr_factor/thirtee_yr_factor as y \r\n" + 
 				"  from `sovereign_data`\r\n" + 
-				"  where subgroup_id=:subGroupId",
+				"  where subgroup_id=:subGroupId "
+				+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 			      nativeQuery = true)
     public List<DataGraphDTO> findGraphDataForTenOverThirteeBySubroupId(@Param("subGroupId") long subGroupId);
 	 
-	 @Query(value = "select five_yr_factor/thirtee_yr_factor as y,\r\n" + 
-				"       refer_date as x\r\n" + 
+	 @Query(value = "select STR_TO_DATE(refer_date,'%d-%m-%Y') as x ,\r\n" + 
+				"    five_yr_factor/thirtee_yr_factor as y  \r\n" + 
 				"  from `sovereign_data`\r\n" + 
-				"  where subgroup_id=:subGroupId",
+				"  where subgroup_id=:subGroupId "
+				+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 			      nativeQuery = true)
      public List<DataGraphDTO> findGraphDataForFiveOverThirteeBySubroupId(@Param("subGroupId") long subGroupId);
 	 
-	 @Query(value = "select two_yr_factor as y,\r\n" + 
-				"       refer_date as x\r\n" + 
+	 @Query(value = "select STR_TO_DATE(refer_date,'%d-%m-%Y') as x,\r\n" + 
+				"      two_yr_factor as y\r\n" + 
 				"  from `sovereign_data`\r\n" + 
-				"  where subgroup_id=:subGroupId",
+				"  where subgroup_id=:subGroupId "
+				+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 			      nativeQuery = true)
      public List<DataGraphDTO> findGraphDataForTwoBySubroupId(@Param("subGroupId") long subGroupId);
 	 
-	 @Query(value = "select ten_yr_factor as y,\r\n" + 
-				"       refer_date as x\r\n" + 
+	 @Query(value = "select  STR_TO_DATE(refer_date,'%d-%m-%Y') as x, " + 
+				"    ten_yr_factor as y " + 
 				"  from `sovereign_data`\r\n" + 
-				"  where subgroup_id=:subGroupId",
+				"  where subgroup_id=:subGroupId "
+				+ "order by STR_TO_DATE(refer_date,'%d-%m-%Y')",
 			      nativeQuery = true)
      public List<DataGraphDTO> findGraphDataForTenBySubroupId(@Param("subGroupId") long subGroupId);
 
