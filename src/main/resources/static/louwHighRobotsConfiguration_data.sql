c33847abcddbc7f3509af1cfeb65c9dbc2711194
jean.s <jean.s@euriskomobility.com>
2021-01-22 19:23:27 +0200

    add test data for robots-configuration

---
+++ src/main/resources/static/louwHighRobotsConfiguration_data.sql
@@ -0,0 +1,12 @@
+insert into bourse.low_high_robots_configuration
+select 1
+      ,'USA-30'
+      ,'USA-30'
+      ,true
+      ,null
+      ,null
+      ,'the {ColumnTitle} is at a {Time} {RuleResult} of {P0}'
+      ,'2 weeks'
+      ,'2 weeks' 
+from dual;
+ 
