65fed8eafec958e813967b18477dfe8b2048eb3a
micha-mn <57522990+micha-mn@users.noreply.github.com>
2021-01-28 22:30:45 +0200

    Update script.sql

--- src/main/resources/static/script.sql
+++ src/main/resources/static/script.sql
@@ -45,7 +45,7 @@ select 9,'GERMANY',      2,               3,         'YIELD CURVES (C)' from dua
 select 10,'UK',           2,               4,         'YIELD CURVES (C)' from dual union 
 select 11,'ITALY',        2,               5,         'YIELD CURVES (C)' from dual union
 select 12,'SPAIN',        2,               6,         'YIELD CURVES (C)' from dual union
-select 13,'FRA_GER',      3,               1,         'SOVEREIGN YIELD CROSSES (C)' from dual union
+select 13,'FRA-GER',      3,               1,         'SOVEREIGN YIELD CROSSES (C)' from dual union
 select 14,'ITA-GER',      3,               2,         'SOVEREIGN YIELD CROSSES (C)' from dual union
 select 15,'SPN-GER',      3,               3,         'SOVEREIGN YIELD CROSSES (C)' from dual union
 select 16,'UK-GER',       3,               4,         'SOVEREIGN YIELD CROSSES (C)' from dual union
@@ -99,7 +99,8 @@ CREATE TABLE bourse.column_configuration (
   y_axis_format varchar(255) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
+alter table bourse.column_configuration add factor varchar(255);
+alter table bourse.column_configuration add descwitoutfactor varchar(255);
 insert into bourse.column_configuration(id,description,group_id,subgroup_id)
 select (@row_number:=@row_number + 1) AS  id,
        concat(p1.description,'-',p2.description),
@@ -109,4 +110,119 @@ select (@row_number:=@row_number + 1) AS  id,
        bourse.factor p2,
        (SELECT @row_number:=0) AS t
 where p1.group_id = p2.group_id;
+
+update bourse.column_configuration 
+   set factor = SUBSTRING_INDEX(SUBSTRING_INDEX(description, '-', 2), '-', -1)
+ where group_id = 1 ;
+ 
+update bourse.column_configuration
+   set factor = SUBSTRING_INDEX(SUBSTRING_INDEX(description, '-', 2), '-', -1)
+ where group_id = 2 ;
+ 
+ update bourse.column_configuration
+   set factor = SUBSTRING_INDEX(SUBSTRING_INDEX(description, '-', 3), '-', -1)
+ where group_id = 3 ;
+ 
+ /*must be done for other groups*/
+   
+update bourse.column_configuration 
+set descwitoutfactor = replace(description,concat('-',factor),'');
+
+
+
+
+create table bourse.updateDisplay as select tab.*
+ from(
+select 'USA-2' as description, 'USA 2-yr Benchmark YIELDs'   as display                      from dual union
+select 'USA-5', 'USA 5-yr Benchmark YIELD'                          from dual union
+select 'USA-10', 'USA 10-yr Benchmark YIELD'                        from dual union
+select 'USA-30', 'USA 30-yr Benchmark YIELD'                        from dual union
+select 'FRANCE-2', 'France 2-yr Benchmark YIELD'                    from dual union
+select 'FRANCE-5', 'France 5-yr Benchmark YIELD'                    from dual union
+select 'FRANCE-10', 'France 10-yr Benchmark YIELD'                  from dual union
+select 'FRANCE-30', 'France 30-yr Benchmark YIELD'                  from dual union
+select 'GERMANY-2', 'Germany 2-yr Benchmark YIELDs'                 from dual union
+select 'GERMANY-5', 'Germany 5-yr Benchmark YIELD'                  from dual union
+select 'GERMANY-10', 'Germany 10-yr Benchmark YIELD'                from dual union
+select 'GERMANY-30', 'Germany 30-yr Benchmark YIELD'                from dual union
+select 'UK-2', 'Uk 2-yr Benchmark YIELD'                            from dual union
+select 'UK-5', 'Uk 5-yr Benchmark YIELD'                            from dual union
+select 'UK-10', 'Uk 10-yr Benchmark YIELD'                          from dual union
+select 'UK-30', 'Uk 30-yr Benchmark YIELD'                          from dual union
+select 'ITALY-2', 'Italy 2-yr Benchmark YIELD'                      from dual union
+select 'ITALY-5', 'Italy 5-yr Benchmark YIELD'                      from dual union
+select 'ITALY-10', 'Italy 10-yr Benchmark YIELD'                    from dual union
+select 'ITALY-30', 'Italy 30-yr Benchmark YIELD'                    from dual union
+select 'SPAIN-2', 'Spain 2-yr Benchmark YIELD'                      from dual union
+select 'SPAIN-5', 'Spain 5-yr Benchmark YIELD'                      from dual union
+select 'SPAIN-10', 'Spain 10-yr Benchmark YIELD'                    from dual union
+select 'SPAIN-30', 'Spain 30-yr Benchmark YIELD'                    from dual union
+select 'USA-2/5', 'USA 2\'s/5\'s yield CURVE'                       from dual union
+select 'USA-2/10', 'USA 2\'s/10\'s yield CURVE'                     from dual union
+select 'USA-5/10', 'USA 5\'s/10\'s yield CURVE'                     from dual union
+select 'USA-5/30', 'USA 5\'s/30\'s yield CURVE'                     from dual union
+select 'USA-10/30', 'USA 10\'s/30\'s yield CURVE'                   from dual union
+select 'FRANCE-2/5', 'France 2\'s/5\'s yield CURVE'                 from dual union
+select 'FRANCE-2/10', 'France 2\'s/10\'s yield CURVE'               from dual union
+select 'FRANCE-5/10', 'France 5\'s/10\'s yield CURVE'               from dual union
+select 'FRANCE-5/30', 'France 5\'s/30\'s yield CURVE'               from dual union
+select 'FRANCE-10/30', 'France 10\'s/30\'s yield CURVE'             from dual union
+select 'GERMANY-2/5', 'Germany 2\'s/5\'s yield CURVE'               from dual union
+select 'GERMANY-2/10', 'Germany 2\'s/10\'s yield CURVE'             from dual union
+select 'GERMANY-5/10', 'Germany 5\'s/10\'s yield CURVE'             from dual union
+select 'GERMANY-5/30', 'Germany 5\'s/30\'s yield CURVE'             from dual union
+select 'GERMANY-10/30', 'Germany 10\'s/30\'s yield CURVE'           from dual union
+select 'UK-2/5', 'Uk 2\'s/5\'s yield CURVE'                         from dual union
+select 'UK-2/10', 'Uk 2\'s/10\'s yield CURVE'                       from dual union
+select 'UK-5/10', 'Uk 5\'s/10\'s yield CURVE'                       from dual union
+select 'UK-5/30', 'Uk 5\'s/30\'s yield CURVE'                       from dual union
+select 'UK-10/30', 'Uk 10\'s/30\'s yield CURVE'                     from dual union
+select 'ITALY-2/5', 'Italy 2\'s/5\'s yield CURVE'                   from dual union
+select 'ITALY-2/10', 'Italy 2\'s/10\'s yield CURVE'                 from dual union
+select 'ITALY-5/10', 'Italy 5\'s/10\'s yield CURVE'                 from dual union
+select 'ITALY-5/30', 'Italy 5\'s/30\'s yield CURVE'                 from dual union
+select 'ITALY-10/30', 'Italy 10\'s/30\'s yield CURVE'               from dual union
+select 'SPAIN-2/5', 'Spain 2\'s/5\'s yield CURVE'                   from dual union
+select 'SPAIN-2/10', 'Spain 2\'s/10\'s yield CURVE'                 from dual union
+select 'SPAIN-5/10', 'Spain 5\'s/110\'s yield CURVE'                from dual union
+select 'SPAIN-5/30', 'Spain 5\'s/30\'s yield CURVE'                 from dual union
+select 'SPAIN-10/30', 'Spain 10\'s/30\'s yield CURVE'               from dual union
+select 'FRA_GER-2', 'FRA-GER 2-yr yield CROSS'                      from dual union
+select 'FRA_GER-5', 'FRA-GER 5-yr yield CROSS'                      from dual union
+select 'FRA_GER-10', 'FRA-GER 10-yr yield CROSS'                    from dual union
+select 'FRA_GER-30', 'FRA-GER 30-yr yield CROSS'                    from dual union
+select 'ITA-GER-2', 'ITA-GER 2-yr yield CROSS'                      from dual union
+select 'ITA-GER-5', 'ITA-GER 5-yr yield CROSS'                      from dual union
+select 'ITA-GER-10', 'ITA-GER 10-yr yield CROSS'                    from dual union
+select 'ITA-GER-30', 'ITA-GER 30-yr yield CROSS'                    from dual union
+select 'SPN-GER-2', 'SPN-GER 2-yr yield CROSS'                      from dual union
+select 'SPN-GER-5', 'SPN-GER 5-yr yield CROSS'                      from dual union
+select 'SPN-GER-10', 'SPN-GER 10-yr yield CROSS'                    from dual union
+select 'SPN-GER-30', 'SPN-GER 30-yr yield CROSS'                    from dual union
+select 'UK-GER-2', 'UK-GER 2-yr yield CROSS'                        from dual union
+select 'UK-GER-5', 'UK-GER 5-yr yield CROSS'                        from dual union
+select 'UK-GER-10', 'UK-GER 10-yr yield CROSS'                      from dual union
+select 'UK-GER-30', 'UK-GER 30-yr yield CROSS'                      from dual union
+select 'USA-GER-2', 'USA-GER 2-yr yield CROSS'                      from dual union
+select 'USA-GER-5', 'USA-GER 5-yr yield CROSS'                      from dual union
+select 'USA-GER-10', 'USA-GER 10-yr yield CROSS'                    from dual union
+select 'USA-GER-30', 'USA-GER 30-yr yield CROSS'                    from dual union
+select 'USA-UK-2', 'USA-UK 2-yr yield CROSS'                        from dual union
+select 'USA-UK-5', 'USA-UK 5-yr yield CROSS'                        from dual union
+select 'USA-UK-10', 'USA-UK 10-yr yield CROSS'                      from dual union
+select 'USA-UK-30', 'USA-UK 30-yr yield CROSS'                      from dual union
+select 'ITA-FRA-2', 'ITA-FRA 2-yr yield CROSS'                      from dual union
+select 'ITA-FRA-5', 'ITA-FRA 5-yr yield CROSS'                      from dual union
+select 'ITA-FRA-10', 'ITA-FRA 10-yr yield CROSS'                    from dual union
+select 'ITA-FRA-30', 'ITA-FRA 30-yr yield CROSS'                    from dual union
+select 'ITA-SPN-2', 'ITA-SPN 2-yr yield CROSS'                      from dual union
+select 'ITA-SPN-5', 'ITA-SPN 5-yr yield CROSS'                      from dual union
+select 'ITA-SPN-10', 'ITA-SPN 10-yr yield CROSS'                    from dual union
+select 'ITA-SPN-30', 'ITA-SPN 30-yr yield CROSS'                    from dual)tab;
+
+
+UPDATE bourse.column_configuration
+INNER JOIN bourse.updateDisplay ON bourse.column_configuration.description = bourse.updateDisplay.description
+SET bourse.column_configuration.display_description 
+= bourse.updateDisplay.display;
 
