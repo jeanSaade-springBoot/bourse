436d9bc2fe625f7dfc1158f2807ffc33f9b63ab0
jean.s <jean.s@euriskomobility.com>
2021-03-01 19:53:55 +0200

    make jar properties

--- src/main/java/com/bourse/BourseApplication.java
+++ src/main/java/com/bourse/BourseApplication.java
@@ -2,12 +2,18 @@ package com.bourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BourseApplication {
+public class BourseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BourseApplication.class, args);
 	}
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(BourseApplication.class);
+	    }
 
 }
