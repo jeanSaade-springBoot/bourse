c8a0f8062e8bc6bfa769bb28cd290a569a421095
jeanSaade-SpBt <57522990+micha-mn@users.noreply.github.com>
2022-09-21 16:29:05 +0300

    LATEST

--- src/main/java/com/bourse/controllers/RobotsController.java
+++ src/main/java/com/bourse/controllers/RobotsController.java
@@ -1,12 +1,8 @@
 package com.bourse.controllers;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.bourse.domain.News;
-import com.bourse.domain.RobotInitializer;
-import com.bourse.dto.RobotInitializerDTO;
 import com.bourse.service.RobotInitializerService;
 import com.bourse.service.RobotService;
 @RestController
@@ -31,13 +24,11 @@ public class RobotsController {
 	@Autowired
 	private final RobotInitializerService robotInitializerService;
 	
-	private final RestTemplate restTemplate;
 	
-	public RobotsController(RobotService robotService,RobotInitializerService robotInitializerService,RestTemplate restTemplate)
+	public RobotsController(RobotService robotService,RobotInitializerService robotInitializerService)
 	{
 		this.robotService = robotService;
 		this.robotInitializerService = robotInitializerService;
-		this.restTemplate = restTemplate;
 	}
 	
 	@PostMapping(value = "publishNews", produces = "application/json;charset=UTF-8")
@@ -53,43 +44,7 @@ public class RobotsController {
     }
 	@GetMapping(value = "callRobotsAsync/{robotName}", produces = "application/json;charset=UTF-8")
     public  ResponseEntity<HttpStatus> callRobotsAsync(@PathVariable String robotName){
-		List<RobotInitializer> listOfRobots =  robotInitializerService.getRobotInitializerByRobotName(robotName);
-		/*for (int i = 0; i < listOfRobots.size(); i++) {
-			  try {
-			    	 RobotInitializerDTO robotdto = RobotInitializerDTO.builder()
-			                 .columnName(listOfRobots.get(i).getColumnName())
-			                 .robotName(listOfRobots.get(i).getRobotName())
-			                 .build();
-			    	 
-		 		    HttpHeaders headers = new HttpHeaders();
-		    	    HttpEntity<RobotInitializerDTO> requestEntity = new HttpEntity<>(robotdto, headers);
-		    	    
-		    	    restTemplate.exchange("http://localhost:9090/databseSynchro/runrobot", HttpMethod.POST, requestEntity, RobotInitializerDTO.class);
-			    	    
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}*/
-		 listOfRobots.stream()
-			.parallel()
-			.forEach(myObject -> {
-				try {
-					 RobotInitializerDTO robotdto = RobotInitializerDTO.builder()
-			                 .columnName(myObject.getColumnName())
-			                 .robotName(myObject.getRobotName())
-			                 .build();
-					    HttpHeaders headers = new HttpHeaders();
-			    	    HttpEntity<RobotInitializerDTO> requestEntity = new HttpEntity<>(robotdto, headers);
-			    	    
-			    	    restTemplate.exchange("http://localhost:9090/databseSynchro/runrobot", HttpMethod.POST, requestEntity, RobotInitializerDTO.class);
-				    
-			    	    System.out.println("excuting Robot for column: "+ myObject.getRobotName() +" "+ myObject.getColumnName()+" time:"+new Date());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			});
+		robotInitializerService.callRobotsAsync(robotName);
 		
 		// robotService.triggerRobotAsync(listOfRobots);
 		return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/com/bourse/service/RobotInitializerService.java
+++ src/main/java/com/bourse/service/RobotInitializerService.java
@@ -1,20 +1,62 @@
 package com.bourse.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.bourse.domain.RobotInitializer;
+import com.bourse.dto.RobotInitializerDTO;
 import com.bourse.repositories.RobotInitializerRepository;
 @Service
 public class RobotInitializerService {
 
 	@Autowired
 	RobotInitializerRepository robotInitializerRepository;
+
+	private final RestTemplate restTemplate;
+	public RobotInitializerService(RestTemplate restTemplate)
+	{
+		this.restTemplate = restTemplate;
+	}
 	
 	public List<RobotInitializer> getRobotInitializerByRobotName(String robotName)
 	{
 		return robotInitializerRepository.findByRobotName(robotName);
+	}
+	
+	public void callRobotsAsync(String robotName){
+		List<RobotInitializer> listOfRobots =  getRobotInitializerByRobotName(robotName);
+		
+		 listOfRobots.stream()
+			.parallel()
+			.forEach(myObject -> {
+				try {
+					 RobotInitializerDTO robotdto = RobotInitializerDTO.builder()
+			                 .columnName(myObject.getColumnName())
+			                 .robotName(myObject.getRobotName())
+			                 .build();
+					  
+			    	   
+				    
+			    	    System.out.println("excuting Robot for column: "+ myObject.getRobotName() +" "+ myObject.getColumnName()+" time:"+new Date());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+	}
+	
+	@Async
+	public void executeRObots(RobotInitializerDTO robotdto){
+		 HttpHeaders headers = new HttpHeaders();
+  	     HttpEntity<RobotInitializerDTO> requestEntity = new HttpEntity<>(robotdto, headers);
+		 restTemplate.exchange("http://localhost:9090/databseSynchro/runrobot", HttpMethod.POST, requestEntity, RobotInitializerDTO.class);
 	}
 }
