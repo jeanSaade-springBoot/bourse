99f6317e3d2ad861fcbe6c2de86413185c49bf60
micha-mn <57522990+micha-mn@users.noreply.github.com>
2023-08-03 17:35:39 +0300

    latest

--- src/main/resources/static/js/bourse/ecbQeLiquidity.js
+++ src/main/resources/static/js/bourse/ecbQeLiquidity.js
@@ -573,18 +573,18 @@ function drawGraph() {
 					maxvalue = max;
 					var value1 = getMarginLenght(min1); 
 					var value2 = getMarginLenght(min2); 
-					
-					notDecimal=yaxisformat[1];
-					nbrOfDigits=yaxisformat[0];
+						
+					var yaxisformat0 = getFormat(response[0].config.yAxisFormat);
+                    var yaxisformat1 = getFormat(response[1].config.yAxisFormat);
+                  
+					notDecimal=yaxisformat0[1];
+					nbrOfDigits=yaxisformat0[0];
 					notDecimal1=yaxisformat1[1];
 					nbrOfDigits1=yaxisformat1[0];
 					
 					chartColor = response[0].config.chartColor;
 					chartTransparency=response[0].config.chartTransparency;
-					
-					var yaxisformat0 = getFormat(response[0].config.yAxisFormat);
-                    var yaxisformat1 = getFormat(response[1].config.yAxisFormat);
-                    
+				  
 					var chartConfigSettings={functionId:functionId+1,
 											 isDecimal:isdecimal,
 											 yAxisFormat0:yaxisformat0,
@@ -740,9 +740,10 @@ function drawGraph() {
 					//maxvalue = parseFloat((Math.floor(max * 20) / 20).toFixed(2));
 					minvalue = min;
 					maxvalue = max;
-					notDecimal=yaxisformat[1];
-			    	nbrOfDigits=yaxisformat[0];
 					var yaxisformat0 = getFormat(response[0].config.yAxisFormat);
+					
+					notDecimal=yaxisformat0[1];
+			    	nbrOfDigits=yaxisformat0[0];
 					chart.updateOptions({
 						xaxis: {
 					labels: {
@@ -952,13 +953,15 @@ function drawGraph() {
 							//maxvalue = parseFloat((Math.floor(max * 20) / 20).toFixed(2));
 							minvalue = min;
 							maxvalue = max;
+							
+							var yaxisformat = getFormat(response[0].config.yAxisFormat);
+							
 							notDecimal=yaxisformat[1];
 					        nbrOfDigits=yaxisformat[0];
 							
 							
 							var getFormatResult0 = getFormat(response[0].config.dataFormat);
-					       	var yaxisformat = getFormat(response[0].config.yAxisFormat);
-							var chartConfigSettings={functionId:functionId+1,
+					        var chartConfigSettings={functionId:functionId+1,
 											 isDecimal:isdecimal,
 											 yAxisFormat:yaxisformat,
 											 fontSize:fontsize,
--- src/main/resources/static/js/bourse/energy.js
+++ src/main/resources/static/js/bourse/energy.js
@@ -617,17 +617,17 @@ function drawGraph() {
 					var value1 = getMarginLenght(min1); 
 					var value2 = getMarginLenght(min2); 
 					
-					notDecimal=yaxisformat[1];
-					nbrOfDigits=yaxisformat[0];
+					var yaxisformat0 = getFormat(response[0].config.yAxisFormat);
+                    var yaxisformat1 = getFormat(response[1].config.yAxisFormat);
+                    
+					notDecimal=yaxisformat0[1];
+					nbrOfDigits=yaxisformat0[0];
 					notDecimal1=yaxisformat1[1];
 					nbrOfDigits1=yaxisformat1[0];
 					
 					chartColor = response[0].config.chartColor;
 					chartTransparency=response[0].config.chartTransparency;
 					
-					var yaxisformat0 = getFormat(response[0].config.yAxisFormat);
-                    var yaxisformat1 = getFormat(response[1].config.yAxisFormat);
-                    
 					var chartConfigSettings={functionId:functionId+1,
 											 isDecimal:isdecimal,
 											 yAxisFormat0:yaxisformat0,
@@ -782,10 +782,10 @@ function drawGraph() {
 					//maxvalue = parseFloat((Math.floor(max * 20) / 20).toFixed(2));
 					minvalue = min;
 					maxvalue = max;
-					notDecimal=yaxisformat[1];
-			    	nbrOfDigits=yaxisformat[0];
 					var yaxisformat0 = getFormat(response[0].config.yAxisFormat);
-
+                    notDecimal=yaxisformat0[1];
+			    	nbrOfDigits=yaxisformat0[0];
+					
 					chart.updateOptions({
 						extra: {
 							isDecimal: isdecimal,
@@ -966,6 +966,8 @@ function drawGraph() {
 							//maxvalue = parseFloat((Math.floor(max * 20) / 20).toFixed(2));
 							minvalue = min;
 							maxvalue = max;
+							var yaxisformat = getFormat(response[0].config.yAxisFormat);
+							
 							notDecimal=yaxisformat[1];
 					        nbrOfDigits=yaxisformat[0];
 							
